60c383d3965c76d7cd4c86f007268a6b4e7af5ef
Toxich <@dimentr.anton@gmail.com>
2024-10-09 13:24:42 +0300

    feat: jest snapshot-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,34 @@
 // Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+ import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
     // Write your test here
+    const testList= ['a','b','c','d']
+    const expectedOutput = {
+      value: 'a',
+      next: {
+        value: 'b',
+        next: {
+          value: 'c',
+          next: {
+            value: 'd',
+            next: {
+              value: null,
+              next: null,
+            },
+          },
+        },
+      },
+    };
+    expect(generateLinkedList(testList)).toStrictEqual(expectedOutput);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
     // Write your test here
+    const testData = [1,2,3,4,5,6];
+    expect(generateLinkedList(testData)).toMatchSnapshot()
   });
 });
